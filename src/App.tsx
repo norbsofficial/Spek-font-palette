4e6a724eb9108448ad775e5e26bbe79306460590
norbsofficial <128795591+norbsofficial@users.noreply.github.com>
2025-08-10 22:21:54 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,14 @@ import NotFound from "./pages/NotFound";
 
 
 
-const queryClient = new QueryClient();
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
