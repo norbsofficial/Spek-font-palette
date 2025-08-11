c90c990e5f42ea42c0f3de1b6cd16e1ad79a0b48
norbsofficial <128795591+norbsofficial@users.noreply.github.com>
2025-08-10 21:33:11 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Spek-font-palette/",   
+  plugins: [react()],
+});
+
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
