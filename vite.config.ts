b0da127e8dabe79ce227cf8e867a838a9711444e
norbsofficial <128795591+norbsofficial@users.noreply.github.com>
2025-08-10 21:54:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/Spek-font-palette/",   
+  base: "/Spek-font-palette/", // must exactly match your repo name
   plugins: [react()],
 });
-
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
