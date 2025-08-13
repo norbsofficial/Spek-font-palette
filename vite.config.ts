23262b5c958604aff0c373958f07b17a97c5e4b7
norbsofficial <128795591+norbsofficial@users.noreply.github.com>
2025-08-13 16:05:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Spek-font-palette/",
+  base: "/Spek-font-palette/", // must match repo name exactly, with slashes
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } }, // ok if you added this
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
